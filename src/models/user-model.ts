498e52d9a0d09f2aabc3f8e53f8cc104ce15fa36
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-06 08:52:31 +0000

    user model updated

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,24 +1,36 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isFreelancer: {
+      type: String,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isFreelancer: {
-    type: String,
-    required: true,
-    default: false,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 const User = mongoose.model('User', userSchema);
 
