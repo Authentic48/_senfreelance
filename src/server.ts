cd2a204a80bdf075d7a8a1695a9c67a2cfbe432d
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-06 04:37:12 +0000

    Customerror bad request, notfound done

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,17 @@
-import express from 'express';
+import express, { json, Request, Response } from 'express';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import 'express-async-errors';
 
 const app = express();
+
+app.use(express.json());
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
