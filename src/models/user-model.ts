23fc05e689a6d1485f2660f57dcff5f2df1b781f
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-07 04:13:44 +0000

    Password hashed

--- src/models/user-model.ts
+++ src/models/user-model.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 const userSchema = new mongoose.Schema(
   {
@@ -31,6 +32,14 @@ const userSchema = new mongoose.Schema(
     },
   }
 );
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.Hash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
 
 const User = mongoose.model('User', userSchema);
 
