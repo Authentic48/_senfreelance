0e1d2451c99a0cb7fd1b2ad985d01a60036e5216
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-15 13:27:49 +0000

    freelancers microservice stup with our cluster done

--- freelancers/src/server.ts
+++ freelancers/src/server.ts
@@ -4,11 +4,7 @@ import 'express-async-errors';
 import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError } from '@senefreelance/common';
-import { signupRouter } from './routes/signup';
 import { ConnectDB } from './config/db';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { currentUserRouter } from './routes/current-user';
 
 const app = express();
 app.use(express.json());
@@ -20,11 +16,6 @@ app.use(
   })
 );
 ConnectDB();
-
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(currentUserRouter);
 
 app.all('*', async () => {
   throw new NotFoundError();
