6431d1fd04fe6f6fc6adcb5bc9243cf9c414b0b9
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-16 04:41:00 +0000

    update route done

--- freelancers/src/routes/update.ts
+++ freelancers/src/routes/update.ts
@@ -1,9 +1,47 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@senefreelance/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Freelancer } from '../models/freelancer';
 
 const router = express.Router();
 
-router.put('/api/freelancers/:id', (req: Request, res: Response) => {
-  return res.send({});
-});
+router.put(
+  '/api/freelancers/:id',
+  requireAuth,
+  [
+    body('phone').notEmpty().withMessage('phone is required'),
+    body('bio').notEmpty().withMessage('bio is required'),
+    body('profession').notEmpty().withMessage('profession is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { phone, bio, profession } = req.body;
+
+    const freelancer = await Freelancer.findById(req.params.id);
+
+    if (!freelancer) {
+      throw new NotFoundError();
+    }
+
+    if (freelancer.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    freelancer.set({
+      phone: phone,
+      bio: bio,
+      profession: profession,
+    });
+
+    await freelancer.save();
+
+    return res.status(200).send(freelancer);
+  }
+);
 
 export { router as updateFreelancerRoute };
