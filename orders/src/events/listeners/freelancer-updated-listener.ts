be39071319d793d5ae0bab3772488800ce0baa36
Fall Jr <falljr@Falls-MBP.sibttk.net>
2021-10-23 07:15:27 +0000

    freelancer updated listener done

---
+++ orders/src/events/listeners/freelancer-updated-listener.ts
@@ -0,0 +1,36 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Subjects,
+  Listener,
+  FreelancerUpdatedEvent,
+} from '@senefreelance/common';
+import { queueGroupName } from './queue-group-name';
+import { Freelancer } from '../../models/freelancer';
+
+export class FreelancerUpdatedListener extends Listener<FreelancerUpdatedEvent> {
+  readonly subject = Subjects.FreelancerUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: FreelancerUpdatedEvent['data'], msg: Message) {
+    const { bio, name, email, phone, userId, profession, id } = data;
+
+    const freelancer = await Freelancer.findById(id);
+
+    if (!freelancer) {
+      throw new Error('Freelancer not found');
+    }
+
+    freelancer.set({
+      name,
+      email,
+      phone,
+      userId,
+      profession,
+      bio,
+    });
+
+    await freelancer.save();
+
+    msg.ack();
+  }
+}
